8803518e10c376f3cef3da05d8cddcaacfb57182
Jesse Alves <152413074+Jessebalves@users.noreply.github.com>
2025-11-18 13:34:34 -0500

    Add files via upload

--- KeanRoutes/js/teamMember.js
+++ KeanRoutes/js/teamMember.js
@@ -1,17 +1,20 @@
 const teamMembers = [
-    { img: "KeanRoutes_Images/Jesse.PNG", name: "Jesse Alves", role: "Team Lead/Developer", width: "51%" },
-    { img: "KeanRoutes_Images/Ryan.PNG", name: "Ryan Guillen", role: "UI Designer/Developer", width: "45%" },
-    { img: "KeanRoutes_Images/Bryan.PNG", name: "Bryan Ortega", role: "Lead GPS Developer", width: "52%" },
-    { img: "KeanRoutes_Images/Alex.PNG", name: "Alexander Ribau", role: "Designer/Developer", width: "48%" },
-    { img: "KeanRoutes_Images/Tiff.PNG", name: "Tiffany Choi", role: "Designer/Developer", width: "45%" }
+    { img: "KeanRoutes_Images/Jesse.PNG", name: "Jesse Alves", role: "Team Lead", width: "51%" },
+    { img: "KeanRoutes_Images/Ryan.PNG", name: "Ryan Guillen", role: "UI Designer", width: "48%" },
+    { img: "KeanRoutes_Images/Bryan.PNG", name: "Bryan Ortega", role: "GPS Developer", width: "52%" },
+    { img: "KeanRoutes_Images/Alex.PNG", name: "Alexander Ribau", role: "Data Analyst", width: "48%" },
+    { img: "KeanRoutes_Images/Tiff.PNG", name: "Tiffany Choi", role: "Dev & Tester", width: "45%" }
 ];
 const container = document.getElementById("teamContainer");
 teamMembers.forEach(member => {
     container.innerHTML += `
+    <div class = "dev_contain>
     <div class="person_container">
         <img src="${member.img}" style="border-radius:50%; width:${member.width}; border:3px solid white">
         <p>${member.name}</p>
-        <p><i>${member.role}</i></p>
+        <small><p><i>${member.role}</i></p></small>
+    </div>
     </div>
     `;
-});
+    }
+);
