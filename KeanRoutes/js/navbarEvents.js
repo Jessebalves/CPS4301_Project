fd0708d01093cbe7f688f281be653a91cba4f0d0
Jesse Alves <152413074+Jessebalves@users.noreply.github.com>
2025-11-19 21:16:14 -0500

    created new file called navbarEvents

---
+++ KeanRoutes/js/navbarEvents.js
@@ -0,0 +1,10 @@
+document.getElementById("admin_building").addEventListener("click",function() {
+    const gpsSearchBar = document.getElementById("destination-input");
+    gpsSearchBar.value = "Administration Building, Union, NJ, USA";
+    
+});
+
+document.getElementById("admissions_office").addEventListener("click",function() {
+    const gpsSearchBar = document.getElementById("destination-input");
+    gpsSearchBar.value = "Office of Admissions, Union, NJ, USA";
+});
