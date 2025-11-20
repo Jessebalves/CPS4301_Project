00b0b06a3cb9a4d3be81c7574b2d3802bf78105e
Jesse Alves <152413074+Jessebalves@users.noreply.github.com>
2025-11-20 12:27:48 -0500

    Descriptions on navbar entry click

--- KeanRoutes/js/navbarEvents.js
+++ KeanRoutes/js/navbarEvents.js
@@ -1,10 +1,13 @@
+const gpsSearchBar = document.getElementById("destination-input");
+const navbarEntryDescription = document.getElementById("descriptions");
+
 document.getElementById("admin_building").addEventListener("click",function() {
-    const gpsSearchBar = document.getElementById("destination-input");
     gpsSearchBar.value = "Administration Building, Union, NJ, USA";
-    
+    navbarEntryDescription.innerHTML = "offers assistance with registration, financial questions + tuition.";
 });
 
 document.getElementById("admissions_office").addEventListener("click",function() {
-    const gpsSearchBar = document.getElementById("destination-input");
     gpsSearchBar.value = "Office of Admissions, Union, NJ, USA";
+    navbarEntryDescription.innerHTML = "1000 Morris Ave Ste 1, Union, NJ 07083";
 });
+
