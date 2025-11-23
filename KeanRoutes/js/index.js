e27fe67e59bcbfb414ca82d756121140a51c0304
Jesse Alves <152413074+Jessebalves@users.noreply.github.com>
2025-11-23 03:32:16 -0500

    fixed issues after refactor

--- KeanRoutes/js/index.js
+++ KeanRoutes/js/index.js
@@ -1,4 +1,6 @@
-document.getElementById("navSearch").addEventListener("keyup", function() {
+//Function that runs everytime a character is entered into
+//Navigation Bar Search Bar
+document.getElementById("navigationBarSearchBar").addEventListener("keyup", function() {
   const navKeywordEnteredRaw = this.value.toLowerCase();
   const navKeywordEnteredClean = navKeywordEnteredRaw.trim();
   const navigation_bar = document.getElementById("navigation_bar");
@@ -12,32 +14,39 @@ document.getElementById("navSearch").addEventListener("keyup", function() {
   console.log(li);
  
   for (let i = 0; i < li.length; i++) {
+      //variable associated with title & description of entry
       const navbarText = li[i].textContent;
       const navbarEntry = li[i].closest("li"); 
+      console.log(navbarText);
+      console.log(navbarEntry);
 
+      //If an entry includes word entered by user
+      //We show the entry
       if (navbarText.toLowerCase().includes(navKeywordEnteredClean)) {
       navbarEntry.style.display = "";
-      Building_scroll.style.overflow="visible";
-      Food_scroll.style.overflow = "visible";
-      Events_scroll.style.overflow = "visible";
-      Other_scroll.style.overflow = "visible";
+      //navigation_bar.style.overflow="visible";
+      //Food_scroll.style.overflow = "visible";
+      //Events_scroll.style.overflow = "visible";
+      //Other_scroll.style.overflow = "visible";
       } 
       
+      //If the entry does not contain the word entered by user
+      //Hide the entry
       else {
       navbarEntry.style.display = "none";
       }
   }
 
+  //If the navigation bar searchbar is empty
+  //turn main tabs in navbar back to scroll
   if(navKeywordEnteredClean==""){
     for(let i = 0; i < li.length;i++){
       Building_scroll.style.overflow="scroll";
-      Building_scroll.style.overflowX= "hidden";
+      Building_scroll.style.overflow="hidden";
       Food_scroll.style.overflow = "scroll";
-      Food_scroll.style.overflowX = "hidden";
+      Food_scroll.style.overflow="hidden";
       Events_scroll.style.overflow = "scroll";
-      Events_scroll.style.overflowX = "hidden";
-      Other_scroll.style.overflow = "scroll";
-      Other_scroll.style.overflowX = "hidden";
+      Other_scroll.style.overflow = "hidden";
     }
   }
 
